b56a960fb3689c0d7c8ef010f7298782dacc143e
Ruan Bekker <ruan.ru.bekker@gmail.com>
2021-02-19 15:23:49 +0200

    Create variables.tf

---
+++ aws-fetch-rds-dns-ansible-create/variables.tf
@@ -0,0 +1,14 @@
+variable "rds_admin_username" {
+    type    = string
+    default = "admin"
+}
+
+variable "rds_admin_password" {
+    type    = string
+    default = ""
+}
+
+variable "service_name" {
+    type    = string
+    default = "foobar"
+}
