98f52ddda27ef024b2b2efafd09b3537fc96a2f0
Ruan Bekker <ruan.ru.bekker@gmail.com>
2021-03-04 08:47:06 +0200

    Create variables.tf

---
+++ s3-backend-with-variables/variables.tf
@@ -0,0 +1,8 @@
+variable "aws_profile" {}
+variable "aws_region" {}
+variable "dynamodb_state_table" {}
+variable "environment_name" {}
+variable "platform_type" {}
+variable "platform_type" {}
+variable "s3_state_bucket" {}
+variable "service_name" {}
