57a58d6f77053e1583be133eafe35739c6c6cc36
Ruan Bekker <ruan.ru.bekker@gmail.com>
2021-03-04 08:40:40 +0200

    Create main.tf

---
+++ s3-backend-with-variables/main.tf
@@ -0,0 +1,7 @@
+data "aws_iam_policy" "AmazonEC2ReadOnlyAccess" {
+  arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
+}
+
+data "aws_iam_policy" "AmazonEC2ContainerServiceforEC2Role" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
