9794293635995a9ee6eb6070d1d00121813840d2
Ruan Bekker <ruan.ru.bekker@gmail.com>
2021-03-30 14:43:39 +0200

    Create index.js

---
+++ aws-lambda-api-gw-nodejs/auto-package-monorepo/functions/hello-greeting/index.js
@@ -0,0 +1,22 @@
+'use strict';
+var random_name = require('node-random-name');
+
+function getName(){
+    var response = random_name({ first: true });
+    return response
+}
+
+module.exports.handle = async event => {
+  var name = getName();
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: `Hello ${name}` ,
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
