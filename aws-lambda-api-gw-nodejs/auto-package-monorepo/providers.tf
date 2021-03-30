efba31df496db620eabb3cf474584638e9dfaa26
Ruan Bekker <ruan.ru.bekker@gmail.com>
2021-03-30 14:46:54 +0200

    Create providers.tf

---
+++ aws-lambda-api-gw-nodejs/auto-package-monorepo/providers.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 2.7.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.1.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = ">= 2.1.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "eu-west-1"
+  profile = "test"
+  shared_credentials_file = "~/.aws/credentials"
+}
+
+provider "random" {}
+provider "null" {}
+provider "archive" {}
