ca3b6e0def2ca912f79fe541f266a838e1d1c22d
Ruan Bekker <ruan.ru.bekker@gmail.com>
2021-03-30 14:42:49 +0200

    Create build.tf

---
+++ aws-lambda-api-gw-nodejs/auto-package-monorepo/build.tf
@@ -0,0 +1,27 @@
+locals {
+  build_directory_path = "${path.module}/build"
+  lambda_common_libs_layer_path = "${path.module}/layers/${var.lambda_layer}"
+  lambda_common_libs_layer_zip_name = "${local.build_directory_path}/${var.lambda_layer}.zip"
+}
+
+resource "null_resource" "common_nodejs_layer" {
+  provisioner "local-exec" {
+    working_dir = "${local.lambda_common_libs_layer_path}/nodejs"
+    command = "npm install"
+  }
+}
+
+data "archive_file" "common_libs_layer_package" {
+  type = "zip"
+  source_dir = local.lambda_common_libs_layer_path
+  output_path = local.lambda_common_libs_layer_zip_name
+
+  depends_on = [null_resource.common_nodejs_layer]
+}
+
+resource "aws_lambda_layer_version" "common_nodejs_layer" {
+  layer_name = var.lambda_layer
+  filename = local.lambda_common_libs_layer_zip_name
+  source_code_hash = data.archive_file.common_libs_layer_package.output_base64sha256
+  compatible_runtimes = [var.lambda_runtime]
+}
