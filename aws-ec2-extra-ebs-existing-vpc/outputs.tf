f85672d89f5a02272ccd161abf0bc1f0d0082aee
Ruan Bekker <ruan.ru.bekker@gmail.com>
2021-07-16 17:36:07 +0200

    Update outputs.tf

--- aws-ec2-extra-ebs-existing-vpc/outputs.tf
+++ aws-ec2-extra-ebs-existing-vpc/outputs.tf
@@ -2,3 +2,11 @@ output "ip" {
   description = "The private ipv4 ip address"
   value       = aws_instance.my_instance.private_ip
 }
+
+output "az" {
+  value = random_shuffle.az.result[0]
+}
+
+output "subnet" {
+  value = data.aws_subnet.private.id
+}
