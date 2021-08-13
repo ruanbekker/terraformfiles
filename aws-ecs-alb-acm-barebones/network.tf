6b87205a95f159b0845214d41a8b8cda3529bdc7
Ruan Bekker <ruan.ru.bekker@gmail.com>
2021-08-13 17:07:51 +0200

    Create network.tf

---
+++ aws-ecs-alb-acm-barebones/network.tf
@@ -0,0 +1,62 @@
+data "aws_vpc" "vpc" {
+  default = true
+  tags = {
+    Name = "main"
+  }
+}
+
+data "aws_availability_zones" "az" {
+  state = "available"
+  filter {
+    name   = "region-name"
+    values = [var.aws_region]
+  }
+}
+
+data "aws_subnet" "public_1" {
+  availability_zone = data.aws_availability_zones.az.names[0]
+  vpc_id = data.aws_vpc.vpc.id
+  tags = {
+    Tier = var.public_network_tier
+  }
+}
+
+data "aws_subnet" "public_2" {
+  availability_zone = data.aws_availability_zones.az.names[1]
+  vpc_id = data.aws_vpc.vpc.id
+  tags = {
+    Tier = var.public_network_tier
+  }
+}
+
+data "aws_subnet" "public_3" {
+  availability_zone = data.aws_availability_zones.az.names[2]
+  vpc_id = data.aws_vpc.vpc.id
+  tags = {
+    Tier = var.public_network_tier
+  }
+}
+
+data "aws_subnet" "private_1" {
+  availability_zone = data.aws_availability_zones.az.names[0]
+  vpc_id = data.aws_vpc.vpc.id
+  tags = {
+    Tier = var.private_network_tier
+  }
+}
+
+data "aws_subnet" "private_2" {
+  availability_zone = data.aws_availability_zones.az.names[1]
+  vpc_id = data.aws_vpc.vpc.id
+  tags = {
+    Tier = var.private_network_tier
+  }
+}
+
+data "aws_subnet" "private_3" {
+  availability_zone = data.aws_availability_zones.az.names[2]
+  vpc_id = data.aws_vpc.vpc.id
+  tags = {
+    Tier = var.private_network_tier
+  }
+}
