03bdf50117a5256b7f335fb6fbb7c0becb821842
Ruan Bekker <ruan.ru.bekker@gmail.com>
2021-12-02 07:13:11 +0200

    Create providers.tf

---
+++ aws-ecs-fargate-efs/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      version = "~> 3.27"
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "eu-west-1"
+  profile = "terraform"
+  shared_credentials_file = "~/.aws/credentials"
+}
