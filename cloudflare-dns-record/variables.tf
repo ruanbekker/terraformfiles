cd8723992878a2cd3bfc6e4c9d2db366bef8b235
Ruan Bekker <ruan.ru.bekker@gmail.com>
2022-02-14 12:54:45 +0200

    Create variables.tf

---
+++ cloudflare-dns-record/variables.tf
@@ -0,0 +1,14 @@
+variable "cloudflare_email" {
+  type        = string
+  description = "description"
+}
+
+variable "cloudflare_api_token" {
+  type        = string
+  description = "description"
+}
+
+variable "domain_name" {
+  type        = string
+  description = "the cloudflare zone"
+}
