c79f18ff3a697b90b69965eda567cf78c9032708
Ruan Bekker <ruan.ru.bekker@gmail.com>
2022-08-28 08:23:33 +0200

    Create providers.tf

---
+++ kubernetes-namespaces-datasource/providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.13.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  host = "https://172.20.185.188:6443"
+  client_certificate     = base64decode(file("certs/client-cert.pem"))
+  client_key             = base64decode(file("certs/client-key.pem"))
+  cluster_ca_certificate = base64decode(file("certs/cluster-ca-cert.pem"))
+}
