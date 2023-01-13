cd247becaee0d9f6d9a29a108b1ca269e58bb15e
Ruan Bekker <ruan.ru.bekker@gmail.com>
2023-01-13 16:48:01 +0200

    Create variables.tf

---
+++ conditionals-module-random-string/modules/random-string/variables.tf
@@ -0,0 +1,19 @@
+variable "length" {
+  default = 16
+  type    = number
+}
+
+variable "upper" {
+  default = false
+  type    = bool
+}
+
+variable "special" {
+  default = false
+  type    = bool
+}
+
+variable "enabled" {
+  default = false
+  type    = bool
+}
