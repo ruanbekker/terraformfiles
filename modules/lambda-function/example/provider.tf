c30aac68a2dcdafd2ebf8eacb87bbbc4baa9aa85
Ruan Bekker <ruan.bekker@bvnk.com>
2023-12-22 07:35:13 +0200

    :sparkles: add lambda example

---
+++ modules/lambda-function/example/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.23.0"
+    }
+  }
+}
+
+provider "aws" {
+  region                   = "eu-west-1"
+  profile                  = "default"
+  shared_credentials_files = ["~/.aws/credentials"]
+}
